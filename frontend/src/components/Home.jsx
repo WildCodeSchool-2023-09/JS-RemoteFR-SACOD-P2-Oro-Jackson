aaca00c5f9d45b7197c01455b59d317405b36de4
Ijuaa <pierre.colpart@gmail.com>
2023-10-25 11:11:01 +0200

    start created home component

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,26 @@
+function Home() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <p>Home</p>
+          </li>
+          <li>
+            <p>Make a drink</p>
+          </li>
+          <li>
+            <p>Contact</p>
+          </li>
+        </ul>
+      </nav>
+      <div className="video-container">
+        <video autoPlay loop muted className="background-video">
+          <source src="" type="" />
+        </video>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
