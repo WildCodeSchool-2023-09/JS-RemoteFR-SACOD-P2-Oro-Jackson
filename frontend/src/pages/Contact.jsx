0cf667a92478f06e280520db5ba489d78b1bffbe
fgaujard <flavien.gaujard@gmail.com>
2023-10-27 10:43:51 +0200

    contact_form

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,9 +2,29 @@ import Navbar from "./Navbar";
 
 function Contact() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div>
+      <div>
+      <form action="" method="get" class="form">
+                <h3>Laisse moi un message !</h3>
+                <div className="SubForm">
+                    <label id="name">Nom :</label><br/>
+                    <input type="text" name="name" id="name" required/>
+                </div>
+                <div className="SubForm">
+                    <label id="email">Adresse e-mail :</label><br/>
+                    <input type="email" name="email" id="email" required/>
+                </div>
+                <div className="SubForm">
+                    <label id="name">Message :</label><br/>
+                    <input type="text" name="msg" id="msg" required/>
+                </div>
+                <div className="SubFrom">
+                    <input id="submit" type="submit" value="Envoyer"/>
+               </div>
+            </form> 
+      </div>
+    </>
   );
 }
 
