09ab4982b2870ca3ff22d820eeb6a3c9870dbfe1
fgaujard <flavien.gaujard@gmail.com>
2023-10-27 16:06:50 +0200

    contact_form+Css+Fix

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -4,24 +4,24 @@ import "./Contact.scss";
 function Contact() {
   return (
     <>
-      <Navbar />
       <div className="Form">
         <form action="" method="get">
-          <h2>Laisse moi un message !</h2>
-          <div className="SubForm">
-            <label id="name">Nom :</label><br/>
-            <input type="text" name="name" id="name" required/>
+          <h2>Une question ou une recommendation ?</h2>
+          <h3>N’hésites surtout pas à nous contacter, on se fera un plaisir de répondre à ta requête !</h3>
+          <div className="input-group">
+            <input type="text" name="name" className="input-group_input" required/>
+            <label for="name" className="input-group_label">Nom</label>
           </div>
-            <div className="SubForm">
-              <label id="email">Adresse e-mail :</label><br/>
-              <input type="email" name="email" id="email" required/>
+            <div className="input-group">
+              <input type="text" name="email" className="input-group_input" required/>
+              <label for="email" className="input-group_label">Adresse e-mail</label>
             </div>
-            <div className="SubForm">
-              <label id="Message">Votre message :</label><br/>
-              <input type="text" name="msg" id="msg" required/>
+            <div className="input-group">
+              <textarea type="textarea" rows="20" cols="50" name="msg" className="input-group_input" id="msg" required/>
+              <label for="text" className="input-group_label">Votre message</label>
             </div>
-            <div className="SubmitFrom">
-              <input id="submit" type="submit" value="Envoyer"/>
+            <div id="sub_pos">
+              <input className="submit" type="submit" value="Envoyer"/>
             </div>
         </form> 
       </div>
