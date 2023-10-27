b023d4976937ae1b0676bfbd76bd70171ea11902
fgaujard <flavien.gaujard@gmail.com>
2023-10-27 11:17:47 +0200

    contact_form+Css

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,28 +1,29 @@
 import Navbar from "./Navbar";
+import "./Contact.scss";
 
 function Contact() {
   return (
     <>
       <Navbar />
-      <div>
-      <form action="" method="get" class="form">
-                <h3>Laisse moi un message !</h3>
-                <div className="SubForm">
-                    <label id="name">Nom :</label><br/>
-                    <input type="text" name="name" id="name" required/>
-                </div>
-                <div className="SubForm">
-                    <label id="email">Adresse e-mail :</label><br/>
-                    <input type="email" name="email" id="email" required/>
-                </div>
-                <div className="SubForm">
-                    <label id="name">Message :</label><br/>
-                    <input type="text" name="msg" id="msg" required/>
-                </div>
-                <div className="SubFrom">
-                    <input id="submit" type="submit" value="Envoyer"/>
-               </div>
-            </form> 
+      <div className="Form">
+        <form action="" method="get">
+          <h2>Laisse moi un message !</h2>
+          <div className="SubForm">
+            <label id="name">Nom :</label><br/>
+            <input type="text" name="name" id="name" required/>
+          </div>
+            <div className="SubForm">
+              <label id="email">Adresse e-mail :</label><br/>
+              <input type="email" name="email" id="email" required/>
+            </div>
+            <div className="SubForm">
+              <label id="Message">Votre message :</label><br/>
+              <input type="text" name="msg" id="msg" required/>
+            </div>
+            <div className="SubmitFrom">
+              <input id="submit" type="submit" value="Envoyer"/>
+            </div>
+        </form> 
       </div>
     </>
   );
