fe86a600072f51c57a9d0151681868ba1f225df4
ghispell <ghis.pell@hotmail.fr>
2023-10-27 11:25:07 +0200

    Test de l'api

--- frontend/src/pages/MakeADrink.jsx
+++ frontend/src/pages/MakeADrink.jsx
@@ -1,11 +1,27 @@
+import axios from "axios";
+import { useState } from "react";
 import Navbar from "./Navbar";
 import MyCarousel from "../components/Carousel";
 
 function MakeADrink() {
+  const API = "https://www.thecocktaildb.com/api/json/v2/9973533/random.php";
+  const [cocktail, setCocktail] = useState();
+
+  const getCocktail = () => {
+    axios.get(API).then((response) => {
+      setCocktail(response.data.drinks[0]);
+    });
+  };
   return (
     <div>
       <Navbar />
-      <h1>Voici VARIABLE cocktails à faire avec vos ingrédients</h1>
+      <h1>Voici $VARIABLE cocktails à faire avec vos ingrédients</h1>
+      <div>
+        <button type="button" onClick={getCocktail}>
+          TEST
+        </button>
+        <h2>{cocktail.strDrink}</h2>
+      </div>
       <MyCarousel />
     </div>
   );
