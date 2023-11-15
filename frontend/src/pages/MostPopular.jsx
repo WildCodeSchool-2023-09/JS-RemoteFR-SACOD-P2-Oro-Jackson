f50bdc4161c00f01260b5b509adc857e79d772b7
Ijuaa <pierre.colpart@gmail.com>
2023-11-15 09:50:22 +0100

    fixed MostPopular scss

--- frontend/src/pages/MostPopular.jsx
+++ frontend/src/pages/MostPopular.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
 import "../styles/MostPopular.scss";
+import LikeButton from "../components/Like";
+import shareButton from "../assets/share.svg";
 
 function MostPopular() {
   const responsive = {
@@ -24,20 +26,47 @@ function MostPopular() {
 
   const items = cocktails.map((cocktail, index) => (
     <div className="item" data-value={index} key={cocktail.idDrink}>
-      <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
+      <img
+        src={cocktail.strDrinkThumb}
+        alt={cocktail.strDrink}
+        className="cardimg"
+      />
       <h2>{cocktail.strDrink}</h2>
-      <div className="ingredient">
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure1}&nbsp;</p>
         <p>{cocktail.strIngredient1}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure2}&nbsp;</p>
         <p>{cocktail.strIngredient2}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure3}&nbsp;</p>
         <p>{cocktail.strIngredient3}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure4}&nbsp;</p>
         <p>{cocktail.strIngredient4}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure5}&nbsp;</p>
         <p>{cocktail.strIngredient5}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure6}&nbsp;</p>
         <p>{cocktail.strIngredient6}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure7}&nbsp;</p>
         <p>{cocktail.strIngredient7}</p>
+      </div>
+      <div className="measure_ingredient">
+        <p>{cocktail.strMeasure8}&nbsp;</p>
         <p>{cocktail.strIngredient8}</p>
-        <p>{cocktail.strIngredient9}</p>
-        <p>{cocktail.strIngredient10}</p>
-        <p>{cocktail.strIngredient11}</p>
+      </div>
+      <div className="likeAndShareContainer">
+        <LikeButton className="likeIcon" />
+        <img src={shareButton} alt="share" className="shareIcon" />
       </div>
     </div>
   ));
